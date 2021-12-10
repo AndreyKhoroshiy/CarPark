7230a9ded3fdb0c4f036a66f86de126448e78073
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-12-11 00:34:28 +0200

    creating a Driver model

--- vehicle/models.py
+++ vehicle/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
