ce04ea7589f9d1ca1b637b161a1e7b29d2d0809a
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-12-14 03:19:50 +0200

    Adding urls for Vehicle

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from vehicle.views import DriversListCreateView, DriverDetailView
+from vehicle.views import DriversListCreateView, DriverDetailView, VehicleListCreateView, VehicleDetailView
 
 urlpatterns = [
     path('drivers/driver/', DriversListCreateView.as_view()),
     path('drivers/driver/<int:pk>/', DriverDetailView.as_view()),
+    path('vehicles/vehicle/', VehicleListCreateView.as_view()),
+    path('vehicles/vehicle/<int:pk>/', VehicleDetailView.as_view()),
 ]
