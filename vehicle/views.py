30a523e5927e1a38f2d6408314c186a5e08e1e99
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-12-14 03:18:08 +0200

    Adding a view for Vehicle

--- vehicle/views.py
+++ vehicle/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from vehicle.serializers import DriverDetailSerializer
-from vehicle.models import Driver
+from vehicle.serializers import DriverDetailSerializer, VehicleDetailSerializer
+from vehicle.models import Driver, Vehicle
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -17,4 +17,13 @@ class DriverDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Driver.objects.all()
 
 
+class VehicleListCreateView(generics.ListCreateAPIView):
+    serializer_class = VehicleDetailSerializer
+    queryset = Vehicle.objects.all()
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    search_fields = ('driver_id',)
 
+
+class VehicleDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = VehicleDetailSerializer
+    queryset = Vehicle.objects.all()
