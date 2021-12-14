32668c65966bdc18314fd192969762935b2f164b
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-12-14 03:16:32 +0200

    Creating a serializer for a vehicle

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from vehicle.models import Driver
+from vehicle.models import Driver, Vehicle
 
 
 class DriverDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Driver
+        fields = '__all__'
+
+
+class VehicleDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
         fields = '__all__'
