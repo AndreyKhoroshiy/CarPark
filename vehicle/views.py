da14752f1a8e7812b01b703935ec37553645c13f
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-12-14 02:07:23 +0200

    Adding Views to Work with the Driver Model

--- vehicle/views.py
+++ vehicle/views.py
@@ -1,12 +1,20 @@
 from rest_framework import generics
 from vehicle.serializers import DriverDetailSerializer
 from vehicle.models import Driver
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class DriversListCreateView(generics.ListCreateAPIView):
     serializer_class = DriverDetailSerializer
     queryset = Driver.objects.all()
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filter_fields = ('created_at',)
 
+
+class DriverDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = DriverDetailSerializer
+    queryset = Driver.objects.all()
 
 
 
