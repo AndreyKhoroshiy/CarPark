10fa6f7dc92a52c6d93f0eb6f2da98b1a56fbc1d
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-12-14 02:12:17 +0200

    Adding urls to work with the Driver model

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from vehicle.views import DriversListCreateView
+from vehicle.views import DriversListCreateView, DriverDetailView
 
 urlpatterns = [
     path('drivers/driver/', DriversListCreateView.as_view()),
-
+    path('drivers/driver/<int:pk>/', DriverDetailView.as_view()),
 ]
