c81deef98bdede55af7751b4a0903fe3f89a396a
AndreyKhoroshiy <AndreyKhoroshiy@github.com>
2021-12-14 03:14:23 +0200

    Adding Vehicle Model

--- vehicle/models.py
+++ vehicle/models.py
@@ -6,3 +6,13 @@ class Driver(models.Model):
     last_name = models.CharField(verbose_name='Фамилия', max_length=50)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Vehicle(models.Model):
+    driver_id = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True, default=None)
+    make = models.CharField(verbose_name='Дата выпуска', max_length=50)
+    model = models.CharField(verbose_name='Модель', max_length=50)
+    plate_number = models.CharField(verbose_name='Номерной знак', max_length=10)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
